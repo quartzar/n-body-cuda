32f405495f631475da584b33a344dc2d911e2994
quartzar <joey.dorn7@gmail.com>
2022-11-02 23:15:12 +0000

    -

--- NbSystemCUDA.cu
+++ NbSystemCUDA.cu
@@ -729,15 +729,15 @@ void randomiseOrbitals(NBodyICConfig config, float4* m_hPos, float4* m_hVel, int
             m_hVel[1].w = 3.00273e-6f;
     
             // venus
-            // m_hPos[2].y = 0.f;
-            // m_hPos[2].x = 0.723f;
-            // m_hPos[2].z = 0.f;
-            // m_hPos[2].w = 2.447e-6f;
-            //
-            // m_hVel[2].y = 35.02f / (float)KMS_TO_AUD;
-            // m_hVel[2].x = 0.f;
-            // m_hVel[2].z = 0.f;
-            // m_hVel[2].w = 2.447e-6f;
+            m_hPos[2].y = 0.f;
+            m_hPos[2].x = 0.723f;
+            m_hPos[2].z = 0.f;
+            m_hPos[2].w = 2.447e-6f;
+
+            m_hVel[2].y = 35.02f / (float)KMS_TO_AUD;
+            m_hVel[2].x = 0.f;
+            m_hVel[2].z = 0.f;
+            m_hVel[2].w = 2.447e-6f;
             
         }
             break;
