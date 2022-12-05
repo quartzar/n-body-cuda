18967993b892455136fd90bc76887ac1e1cf787f
quartzar <joey.dorn7@gmail.com>
2022-12-05 13:25:01 +0000

    ~

---
+++ cmake-build-debug/CMakeFiles/orbiterv6.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/quartzar/Documents/repos/n-body-cuda/NbKernel_N2.cu" "CMakeFiles/orbiterv6.dir/NbKernel_N2.cu.o" "gcc" "CMakeFiles/orbiterv6.dir/NbKernel_N2.cu.o.d"
+  "/home/quartzar/Documents/repos/n-body-cuda/NbSystemCUDA.cu" "CMakeFiles/orbiterv6.dir/NbSystemCUDA.cu.o" "gcc" "CMakeFiles/orbiterv6.dir/NbSystemCUDA.cu.o.d"
+  "/home/quartzar/Documents/repos/n-body-cuda/NbodyRenderer.cpp" "CMakeFiles/orbiterv6.dir/NbodyRenderer.cpp.o" "gcc" "CMakeFiles/orbiterv6.dir/NbodyRenderer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/orbiterv6.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/orbiterv6.dir/NbKernel_N2.cu.o"
+  "CMakeFiles/orbiterv6.dir/NbKernel_N2.cu.o.d"
+  "CMakeFiles/orbiterv6.dir/NbSystemCUDA.cu.o"
+  "CMakeFiles/orbiterv6.dir/NbSystemCUDA.cu.o.d"
+  "CMakeFiles/orbiterv6.dir/NbodyRenderer.cpp.o"
+  "CMakeFiles/orbiterv6.dir/NbodyRenderer.cpp.o.d"
+  "CMakeFiles/orbiterv6.dir/cmake_device_link.o"
+  "orbiterv6"
+  "orbiterv6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA CXX)
+  include(CMakeFiles/orbiterv6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
